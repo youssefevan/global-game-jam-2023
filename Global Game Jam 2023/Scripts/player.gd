ec61cdddfba964cf2c6c372e54839af5d81b39c8
Evan <eyoussef128@gmail.com>
2023-02-04 02:25:48 -0500

    tweeen isnt linear

--- Global Game Jam 2023/Scripts/player.gd
+++ Global Game Jam 2023/Scripts/player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
 func _physics_process(delta):
+	var tween = create_tween().set_trans(Tween.TRANS_LINEAR)
 	if Global.target_city != null:
-		self.global_position = Global.target_city.global_position
+		tween.tween_property(self, "global_position", Global.target_city.global_position, 1)
+		#self.global_position = Global.target_city.global_position
