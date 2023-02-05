dec4a6d2e71fbf0bfc4156f5220bb029b763c3e9
8by4 <aidanquinn147@gmail.com>
2023-02-05 15:45:26 -0500

    linetest and player replace

--- Global Game Jam 2023/Scripts/WinScreen.gd
+++ Global Game Jam 2023/Scripts/WinScreen.gd
@@ -9,3 +9,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+
+
+func _on_Redo_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			get_tree().change_scene("res://Scenes/Preparation.tscn")
